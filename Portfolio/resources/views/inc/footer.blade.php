e7796912b4bed6856f91caf43ecc3a6ade219951
Zach Gabriel S. Estrella <55230769+zachgse@users.noreply.github.com>
2022-07-16 06:35:57 +0800

    Change file

--- Portfolio/resources/views/inc/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-    <p class="mini-center-element">
-        Copyright 2022 Name Here | Bachelor of Science in Information Systems De La Salle-College of Saint Benilde 
-    </p>
-   
-</div>
